91cf746ae69a3abf69b025f5836e939189c30664
amankarn23 <86719761+amankarn23@users.noreply.github.com>
2023-10-29 21:46:50 +0530

    Add files via upload

---
+++ employe.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class employee
+{
+     private String employeid;
+     private String empname;
+     private int basicsalary,HRA,TA,MA,PF,GS,incometax,netsalary;
+     class main 
+    {
+     public static void main(String args[])
+     {
+         employee employeobj=new employee();
+         employeobj.read(); 
+         employeobj.display(); 
+     }
+   }
+     public void read()
+     {
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Enter the employee id");
+        employeid=scan.next();
+        System.out.println("Enter the employee name");
+        empname=scan.next();
+        System.out.println("Enter the basic salary of an employee");
+        basicsalary=scan.nextInt();
+        calculate();
+     }
+     public void calculate()  
+     {
+        HRA=(50/100)*basicsalary;
+        TA=(30/100)*basicsalary;
+        MA=(25/100)*basicsalary;
+        PF=(10/100)*basicsalary;
+        GS=basicsalary+HRA+TA+MA;
+        incometax=(10/100)*GS;
+        netsalary=GS-incometax-PF;
+     }
+     public void display()  
+     {
+        System.out.println("Employeeid  :  "+employeid+"n"+"Employename  :  "+empname+"n"+"Employee basic salary :  "+basicsalary+"n"+"HRA  :  "+HRA+"n"+"TA  :  "+TA+"n"+"MA  :  "+MA+"n"+"PF  :  "+PF+"n"+"GS  :  "+GS+"n"+"Incometax  :  "+incometax+"n"+"netsalary  :  "+netsalary);
+     }
+ 
+}
