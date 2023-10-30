350355f4280a41dcf78e836d836c404838ca2d33
amankarn23 <86719761+amankarn23@users.noreply.github.com>
2023-10-30 23:23:58 +0530

    Add files via upload

---
+++ areaofsquare.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class areaofsquare {
+    public static void main(String args[]){
+    Scanner in=new Scanner(System.in);
+    int a=in.nextInt();
+    int area=a*a;
+    System.out.println("Area="+area);
+    }
+    
+}
